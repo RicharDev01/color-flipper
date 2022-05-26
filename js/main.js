2b84df8b7ad1a6f90fa8a16a23f2b056415179cd
RicharDev01 <ricardopineda587@gmail.com>
2022-05-26 16:45:05 -0600

    [complete] se termina de realizar la funcionalidad de color flipper

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// alert('main js conectado')
+const id = document.getElementById.bind(document)
+
+const contentFlipper = id('color-content-flipper')
+const btnChangeColor = id('btn-change-color')
+
+
+function randomColor(){
+  return Math.floor(Math.random()*256)
+}
+
+function changeColor(){
+  let r = randomColor(), g = randomColor(), b = randomColor()
+
+  let elGlobal = document.documentElement
+  elGlobal.style.setProperty('--background-color', `rgb(${r}, ${g}, ${b})`)
+
+  contentFlipper.textContent = `rgb(${r}, ${g}, ${b})`
+
+}
+
+
+btnChangeColor.addEventListener('click', changeColor)
